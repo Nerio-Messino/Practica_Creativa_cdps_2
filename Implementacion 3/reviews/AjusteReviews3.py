2c0bf812f666b0f9f7de39e8ef6eef683e581714
Nerio Messino <neriomessino14@outlook.es>
2024-01-22 12:11:03 +0100

    no necesitamos este fichero para nada

--- Implementacion 3/reviews/AjusteReviews3.py
+++
@@ -1,4 +0,0 @@
-import subprocess 
-
-#Nos situamos en el directorio correspondiente y ejecutamos el comando que nos piden para ajustar las versiones
-subprocess.call[("docker run --rm -u root -v $(pwd):/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build")]
